0309ba4a525c2bb60761f80a700582c7cbc677ab
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-21 15:37:07 -0400

    add icons, install other material ui components

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,41 +1,39 @@
+import {LinkedIn, GitHub, Instagram, Email} from '@mui/icons-material';
+
 function Links() {
   return (
     <section class="page" id="links">
       <div id="links-container">
         <span class="link">
           <a href="https://www.linkedin.com/in/sarah-delacruz/">
-            <i class="fa-brands fa-linkedin"></i> 
-            LinkedIn
+            <LinkedIn />
           </a>
         </span>
-        <span class="link">
+        {/* <span class="link">
           <a href="https://www.freecodecamp.org/sdc" id="profile-link">
             <i class="fa-brands fa-free-code-camp"></i> 
             freeCodeCamp
           </a>
-        </span>
+        </span> */}
         <span class="link">
           <a href="https://github.com/sarahdeecee">
-            <i class="fa-brands fa-github"></i> 
-            GitHub
+            <GitHub />
           </a>
         </span>
         <span class="link">
           <a href="http://instagram.com/inakasarah">
-            <i class="fa-brands fa-instagram-square"></i> 
-            Instagram
+            <Instagram />
           </a>
         </span>
-        <span class="link">
+        {/* <span class="link">
           <a href="https://www.flickr.com/photos/186883449@N08/">
             <i class="fa-brands fa-flickr"></i> 
             Flickr
           </a>
-        </span>
+        </span> */}
         <span class="link">
           <a href="mailto:sarah.delacruz@gmail.com">
-            <i class="fa-solid fa-envelope"></i> 
-            Email
+            <Email />
           </a>
         </span>
       </div>
