1ea73710b6d8df3244f82014461018d2d5df7dc2
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-21 15:30:47 -0400

    add Links, move About.js into pages folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Links from './components/Links';
 
 function App() {
   return (
     <div className="App">
-
+      <Links />
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,46 @@
+function Links() {
+  return (
+    <section class="page" id="links">
+      <div id="links-container">
+        <span class="link">
+          <a href="https://www.linkedin.com/in/sarah-delacruz/">
+            <i class="fa-brands fa-linkedin"></i> 
+            LinkedIn
+          </a>
+        </span>
+        <span class="link">
+          <a href="https://www.freecodecamp.org/sdc" id="profile-link">
+            <i class="fa-brands fa-free-code-camp"></i> 
+            freeCodeCamp
+          </a>
+        </span>
+        <span class="link">
+          <a href="https://github.com/sarahdeecee">
+            <i class="fa-brands fa-github"></i> 
+            GitHub
+          </a>
+        </span>
+        <span class="link">
+          <a href="http://instagram.com/inakasarah">
+            <i class="fa-brands fa-instagram-square"></i> 
+            Instagram
+          </a>
+        </span>
+        <span class="link">
+          <a href="https://www.flickr.com/photos/186883449@N08/">
+            <i class="fa-brands fa-flickr"></i> 
+            Flickr
+          </a>
+        </span>
+        <span class="link">
+          <a href="mailto:sarah.delacruz@gmail.com">
+            <i class="fa-solid fa-envelope"></i> 
+            Email
+          </a>
+        </span>
+      </div>
+    </section>
+  );
+}
+
+export default Links;
