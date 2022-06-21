5daa786630799c4613b706ba02feb6581d2bfa8d
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-21 16:24:53 -0400

    fix link buttons

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,42 +1,22 @@
-import {LinkedIn, GitHub, Instagram, Email} from '@mui/icons-material';
+import { LinkedIn, GitHub, Instagram, Email } from '@mui/icons-material';
+import { Button } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFlickr, faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons";
 
 function Links() {
   return (
-    <section class="page" id="links">
-      <div id="links-container">
-        <span class="link">
-          <a href="https://www.linkedin.com/in/sarah-delacruz/">
-            <LinkedIn />
-          </a>
-        </span>
-        {/* <span class="link">
-          <a href="https://www.freecodecamp.org/sdc" id="profile-link">
-            <i class="fa-brands fa-free-code-camp"></i> 
-            freeCodeCamp
-          </a>
-        </span> */}
-        <span class="link">
-          <a href="https://github.com/sarahdeecee">
-            <GitHub />
-          </a>
-        </span>
-        <span class="link">
-          <a href="http://instagram.com/inakasarah">
-            <Instagram />
-          </a>
-        </span>
-        {/* <span class="link">
-          <a href="https://www.flickr.com/photos/186883449@N08/">
-            <i class="fa-brands fa-flickr"></i> 
-            Flickr
-          </a>
-        </span> */}
-        <span class="link">
-          <a href="mailto:sarah.delacruz@gmail.com">
-            <Email />
-          </a>
-        </span>
-      </div>
+    <section className="page" id="links">
+      <Button href="https://www.linkedin.com/in/sarah-delacruz" startIcon={<LinkedIn />} />
+      
+      <Button href="https://github.com/sarahdeecee" startIcon={<GitHub />} />
+
+      <Button href="https://www.freecodecamp.org/sdc" startIcon={<FontAwesomeIcon icon={faFreeCodeCamp} />} />
+      
+      <Button href="https://www.instagram.com/inakasarah" startIcon={<Instagram />} />
+      
+      <Button href="https://www.flickr.com/photos/186883449@N08/" startIcon={<FontAwesomeIcon icon={faFlickr} />} />
+
+      <Button href="mailto:sarah.delacruz@gmail.com" startIcon={<Email />} />
     </section>
   );
 }
