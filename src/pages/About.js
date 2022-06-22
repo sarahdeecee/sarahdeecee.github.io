00f190bf13b0673a7784e4514467d236c5dd2f97
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-21 22:31:35 -0400

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,43 +1,50 @@
+import { Typography } from "@mui/material";
+
+const biography = `My name is Sarah and I'm passionate about user experience, efficient code, and making things make sense.
+
+My interest in design and web development started at age 12 when I started learning HTML and CSS from free resources online. I enjoyed creating small websites for my favourite cartoon characters and making custom themes on MySpace.
+
+After an adventurous 5 years working in Japan, I decided to return to Canada and revisit my childhood hobby of creating websites. Now, I'm focused on pursuing web development as a career.`
+
+const funFact = `Fun fact: My older brother gifted me a book titled "How to Do Everything with HTML" in the early 2000's &#8212; I still own it to this day.`;
+
+const certifications = [
+  { title: "Diploma of Web Development",
+    company: "Lighthouse Labs",
+    link: "",
+    companyLink: ""
+  },
+  { title: "Responsive Web Design Certification",
+    company: "freeCodeCamp",
+    link: "https://www.freecodecamp.org/certification/sdc/responsive-web-design",
+    companyLink: ""
+  },
+  { title: "JavaScript Algorithms and Data Structures Certification",
+    company: "freeCodeCamp",
+    link: "https://www.freecodecamp.org/certification/sdc/javascript-algorithms-and-data-structures",
+    companyLink: ""
+  },
+  { title: "Front End Development Libraries",
+    company: "freeCodeCamp",
+    link: "https://www.freecodecamp.org/",
+    companyLink: ""
+  },
+];
+
 function About() {
+
+  const parsedCertifications = certifications.map(certification => <li><strong><a href={certification.link}>{certification.title}</a></strong> from <a href={certification.companyLink}>{certification.company}</a></li>);
+
   return (
     <section className="page" id="about">
       <article>
-        <h2>About Me</h2>
-        <p>
-          My name is Sarah and I'm passionate about user experience, efficient code, and making things make sense.
-        </p>
-        <p>
-          My interest in design and web development started at age 12 when I started learning HTML and CSS from free resources online. I enjoyed creating small websites for my favourite cartoon characters and making custom themes on MySpace.
-        </p>
-        <p>
-          After an adventurous 5 years working in Japan, I decided to return to Canada and revisit my childhood hobby of creating websites. Now, I'm focused on pursuing web development as a career.
-        </p>
-        <p><small>
-          Fun fact: My older brother gifted me a book titled <strong>How to Do Everything with HTML</strong> in the early 2000's &#8212; I still own it to this day.
-        </small></p>
-        <p>Certifications:</p>
+        <Typography variant="h2">About Me</Typography>
+        <Typography variant="body1" style={{ whiteSpace: "pre-line" }}>{biography}</Typography>
+        <Typography variant="body2">Fun fact: My older brother gifted me a book titled <strong>How to Do Everything with HTML</strong> in the early 2000's &#8212; I still own it to this day.</Typography>
+
+        <Typography variant="h3">Certifications:</Typography>
         <ul>
-          <li>
-            <strong>Diploma of Web Development</strong> from Lighthouse Labs
-          </li>
-          <li>
-            <a href="https://www.freecodecamp.org/certification/sdc/responsive-web-design">
-              <strong>Responsive Web Design Certification</strong>
-            </a>
-              from 
-            <a href="https://www.freecodecamp.org/">
-              freeCodeCamp
-            </a>
-          </li>
-          <li>
-            <a href="https://www.freecodecamp.org/certification/sdc/javascript-algorithms-and-data-structures">
-              <strong>JavaScript Algorithms and Data Structures Certification</strong> 
-            </a>
-            from 
-            <a href="https://www.freecodecamp.org/">
-              freeCodeCamp
-            </a>
-          </li>
+          {parsedCertifications}
         </ul>
       </article>
     </section>
