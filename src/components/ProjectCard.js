a0ec7f271eaaa6707035e65cee6c50a679c38db2
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-22 01:26:34 -0400

    implement projects

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,32 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Link, Typography } from "@mui/material";
+
+function ProjectCard(props) {
+  const {title, description, image, deployLink, githubLink} = props;
+
+  const handleProjectCard = e => {
+    console.log(e.target.value);
+  }
+
+  return (
+    <Card variant="outlined" sx={{ height: '100%' }}>
+      <CardActionArea onClick={handleProjectCard}>
+        <CardMedia
+          component="img"
+          class="project-img"
+          image={image}
+          alt={`Screenshot of ${title}`}
+          />
+        <CardContent>
+          <Typography varuant="h5" component="div">{title}</Typography>
+          <Typography varuant="body2" component="p">{description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button href={deployLink}>Link</Button>
+        <Button href={githubLink}>GitHub</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default ProjectCard;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,29 @@
 import { Typography } from "@mui/material";
+import ProjectCard from "../components/ProjectCard";
+
+const projects = [
+  {
+    title: "Portfolio Page",
+    description: "Personal portfolio page designed in HTML, CSS and Bootstrap.",
+    deployLink: "",
+    image: "https://github.com/sarahdeecee/fcc-responsive-projects/blob/main/05-personal-portfolio/data/project-portfolio.png?raw=true",
+  },
+  {
+    title: "Technical Documentation Page",
+    description: "A responsive page showing a sample of Vue documentation.",
+    deployLink: "https://codepen.io/s_dc/details/xxPMgNK",
+    image: "https://github.com/sarahdeecee/fcc-responsive-projects/blob/main/05-personal-portfolio/data/project-doc-page.png?raw=true",
+  }
+]
 
 function Projects() {
+  const parsedProjects = projects.map(project => <ProjectCard {...project} />);
+
   return (
     <section className="page" id="projects">
       <article>
         <Typography variant="h2">Projects</Typography>
+        {parsedProjects}      
       </article>
     </section>
   );
