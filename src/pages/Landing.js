2bf58adb78d0055929a98e19304899124875ddfe
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-22 15:44:29 -0400

    add logos, update landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,27 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function Landing() {
   return (
     <section id="landing">
       <article id="bio">
-        <Typography variant="h4" component="p">Hello! My name is</Typography>
-        <Typography variant="h1" component="p" id="name">Sarah Dela Cruz.</Typography>
-        <Typography variant="h4" component="p">I'm a <strong>web developer</strong> from Ontario, Canada.</Typography>
+        <Grid container direction="row" sx={{justifyContent: "space-between", alignItems: "center"}}>
+          <Grid item>
+            <Box>
+              <Typography variant="h4" component="p">Hello! My name is</Typography>
+              <Typography variant="h1" component="p" id="name" sx={{fontWeight: "400"}}>Sarah Dela Cruz.</Typography>
+              <Typography variant="h4" component="p">I'm a <strong>web developer</strong> from Ontario, Canada.</Typography>
+            </Box>
+          </Grid>
+          <Grid item>
+            <img
+              src="/Pink Sakura Flowers Logo copy.png"
+              srcSet="/Pink Sakura Flowers Logo copy.png"
+              alt="Sakura S Logo"
+              loading="lazy"
+            />
+          </Grid>
+        </Grid>
       </article>
     </section>
   );
