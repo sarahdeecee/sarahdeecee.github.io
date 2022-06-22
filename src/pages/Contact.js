c5f0287967d98a6963658d7962481f95a6201946
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-22 18:15:52 -0400

    update styling, add logo animation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,10 @@
+import { Typography } from "@mui/material";
+
 function Contact() {
   return (
     <section className="page" id="contact">
       <article>
-        <h2>Let's get in touch!</h2>
+        <Typography variant="h2">Let's get in touch!</Typography>
       </article>
     </section>
   );
