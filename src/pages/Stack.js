a609ff7b6fdae3d1df0ee5558acacf49454fb405
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-21 23:17:30 -0400

    refactor stack page

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -1,71 +1,65 @@
 import { Typography } from "@mui/material";
 
+const stack = [
+  { title: "HTML-5",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/html-5.svg",
+  },
+  { title: "CSS-3",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/css-3.svg",
+  },
+  { title: "JavaScript",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/javascript.svg",
+  },
+  { title: "Ruby",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/ruby.svg",
+  },
+  {title: "Node JS",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/nodejs-icon.svg",
+  },
+  {title: "React",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/react.svg",
+  },
+  {title: "Rails",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/rails.svg",
+  },
+  {title: "Postgres",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/postgresql.svg",
+  },
+  {title: "Git",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/git-icon.svg",
+  },
+  {title: "Sass",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/sass.svg",
+  },
+  {title: "Bootstrap",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/bootstrap.svg",
+  },
+  {title: "Material UI",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/material-ui.svg",
+  },
+  {title: "Cypress",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/cypress.svg",
+  },
+  {title: "Jest",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/jest.svg",
+  },
+  {title: "Storybook",
+    src: "https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/storybook-icon.svg",
+  }
+];
+
 function Stack() {
+  const parsedStack = stack.map(stack => <span className="logo-box" data-toggle="tooltip" data-placement="top" title={stack.title}>
+  <img src={stack.src} className="stack-logo" alt={stack.title} />
+  {stack.title}
+  </span>);
+
   return (
     <section className="page" id="stack">
       <article>
         <Typography variant="h2">Tech Stack</Typography>
         <div id="logo-container">
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="HTML">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/html-5.svg" className="stack-logo" alt="HTML-5" />
-            HTML-5
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/css-3.svg" className="stack-logo" alt="CSS-3" />
-            CSS-3
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="JavaScript">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/javascript.svg" className="stack-logo" alt="JavaScript" />
-            JavaScript
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/ruby.svg" className="stack-logo" alt="Ruby" />
-            Ruby
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/nodejs-icon.svg" className="stack-logo" alt="Node JS" />
-            Node JS
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/react.svg" className="stack-logo" alt="ReactJS" />
-            React
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/rails.svg" className="stack-logo" alt="Rails" />
-            Rails
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/postgresql.svg" className="stack-logo" alt="PostgresQL" />
-            Postgres
-          </span>
-          <span className="logo-box">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/git-icon.svg" className="stack-logo" data-toggle="tooltip" data-placement="top" title="Git" alt="Git" />
-            Git
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/sass.svg" className="stack-logo" alt="Sass" />
-            Sass
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/bootstrap.svg" className="stack-logo" alt="Bootstrap" />
-            Bootstrap
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/material-ui.svg" className="stack-logo" alt="Material UI" />
-            Material UI
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/cypress.svg" className="stack-logo" alt="Cypress" />
-            Cypress
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/jest.svg" className="stack-logo" alt="Jest" />
-            Jest
-          </span>
-          <span className="logo-box" data-toggle="tooltip" data-placement="top" title="">
-            <img src="https://raw.githubusercontent.com/sarahdeecee/fcc-responsive-projects/0950210dc1a69eca717ca037b3586d202145c75b/05-personal-portfolio/data/storybook-icon.svg" className="stack-logo" alt="Storybook" />
-            Storybook
-          </span>
+          {parsedStack}
         </div>
       </article>
     </section>
