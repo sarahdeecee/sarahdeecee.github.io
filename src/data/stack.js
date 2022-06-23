93eb85fa2fb42e292cdbf6c0d78e8160a3ee1c80
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-23 01:43:43 -0400

    separate skills by type

---
+++ src/data/stack.js
@@ -0,0 +1,65 @@
+
+const stack = [
+  { title: "HTML-5",
+    src: "./logos/html-5.svg",
+    type: "framework"
+  },
+  { title: "CSS-3",
+    src: "./logos/css-3.svg",
+    type: "framework"
+  },
+  { title: "JavaScript",
+    src: "./logos/javascript.svg",
+    type: "language"
+  },
+  { title: "Ruby",
+    src: "./logos/ruby.svg",
+    type: "language"
+  },
+  {title: "Node JS",
+    src: "./logos/nodejs-icon.svg",
+    type: "framework"
+  },
+  {title: "React",
+    src: "./logos/react.svg",
+    type: "framework"
+  },
+  {title: "Rails",
+    src: "./logos/rails.svg",
+    type: "framework"
+  },
+  {title: "Postgres",
+    src: "./logos/postgresql.svg",
+    type: "database"
+  },
+  {title: "Git",
+    src: "./logos/git-icon.svg",
+    type: "other"
+  },
+  {title: "Sass",
+    src: "./logos/sass.svg",
+    type: "design"
+  },
+  {title: "Bootstrap",
+    src: "./logos/bootstrap.svg",
+    type: "design"
+  },
+  {title: "Material UI",
+    src: "./logos/material-ui.svg",
+    type: "design"
+  },
+  {title: "Cypress",
+    src: "./logos/cypress.svg",
+    type: "testing"
+  },
+  {title: "Jest",
+    src: "./logos/jest.svg",
+    type: "testing"
+  },
+  {title: "Storybook",
+    src: "./logos/storybook-icon.svg",
+    type: "testing"
+  }
+];
+
+export default stack;
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,70 +1,14 @@
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
-
-const stack = [
-  { title: "HTML-5",
-    src: "./logos/html-5.svg",
-    type: "Framework"
-  },
-  { title: "CSS-3",
-    src: "./logos/css-3.svg",
-    type: "Framework"
-  },
-  { title: "JavaScript",
-    src: "./logos/javascript.svg",
-    type: "Language"
-  },
-  { title: "Ruby",
-    src: "./logos/ruby.svg",
-    type: "Language"
-  },
-  {title: "Node JS",
-    src: "./logos/nodejs-icon.svg",
-    type: "Franework"
-  },
-  {title: "React",
-    src: "./logos/react.svg",
-    type: "Franework"
-  },
-  {title: "Rails",
-    src: "./logos/rails.svg",
-    type: "Franework"
-  },
-  {title: "Postgres",
-    src: "./logos/postgresql.svg",
-    type: "Database"
-  },
-  {title: "Git",
-    src: "./logos/git-icon.svg",
-    type: "Other"
-  },
-  {title: "Sass",
-    src: "./logos/sass.svg",
-    type: ""
-  },
-  {title: "Bootstrap",
-    src: "./logos/bootstrap.svg",
-    type: "Design"
-  },
-  {title: "Material UI",
-    src: "./logos/material-ui.svg",
-    type: "Design"
-  },
-  {title: "Cypress",
-    src: "./logos/cypress.svg",
-    type: "Testing"
-  },
-  {title: "Jest",
-    src: "./logos/jest.svg",
-    type: "Testing"
-  },
-  {title: "Storybook",
-    src: "./logos/storybook-icon.svg",
-    type: "Testing"
-  }
-];
+import stack from '../data/stack';
 
 function Skills() {
-  const parsedStack = stack.map(stack => <Card 
+  const languages = stack.filter(elem => elem.type === 'language');
+  const frameworks = stack.filter(elem => elem.type === 'framework');
+  const databases = stack.filter(elem => elem.type === 'database');
+  const designs = stack.filter(elem => elem.type === 'design');
+  const others = stack.filter(elem => elem.type === 'other');
+  
+  const parseStack = array => array.map(stack => <Card 
     key={stack.title}
     variant="outlined"
     className="logo-box"
@@ -88,7 +32,11 @@ function Skills() {
       <article>
         <Typography variant="h2" className="header">Tech Stack</Typography>
         <div id="logo-container">
-          {parsedStack}
+          {parseStack(languages)}
+          {parseStack(frameworks)}
+          {parseStack(databases)}
+          {parseStack(designs)}
+          {parseStack(others)}
         </div>
       </article>
     </section>
