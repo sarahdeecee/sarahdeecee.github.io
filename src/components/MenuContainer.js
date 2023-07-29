ebc4c7300685b91769499034eb6c1fbbc5a6744f
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-07-29 17:46:57 -0400

    clean code

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -1,28 +1,10 @@
-import { ToggleOff, ToggleOn } from '@mui/icons-material';
-import { ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Slide, styled, CssBaseline, useScrollTrigger } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import { useState } from 'react';
-import ThemeToggle from './ThemeToggle';
-import LinksMobile from './LinksMobile';
 import LinksBar from './LinksBar';
-import { linksArr } from '../data/Links';
-
-const drawerWidth = 240;
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-
 
 const linkTextStyle = {fontSize: '16px', fontWeight: 500};
 
 function MenuContainer(props) {
-  // const theme = useTheme();
   const {particles, setParticles} = props;
   const [openLinks, setOpenLinks] = useState(false);
 
