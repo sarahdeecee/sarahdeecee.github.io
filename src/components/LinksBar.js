7c1bcb2abba618352fc307e69c35a213153454ae
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-07-29 17:46:23 -0400

    move toggles to LinksBar component

--- src/components/LinksBar.js
+++ src/components/LinksBar.js
@@ -1,8 +1,40 @@
-import { List } from "@mui/material";
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import { ParticleToggle } from "./ParticleToggle";
+import { linksArr } from "../data/Links";
+import ThemeToggle from "./ThemeToggle";
 
 function LinksBar(props) {
-  const {themeToggle, linksList, openLinks, particles, setParticles} = props;
+  const {openLinks, particles, setParticles} = props;
+
+  const linkTextStyle = {fontSize: '16px', fontWeight: 500};
+
+  const themeToggle = <ThemeToggle key='switch-button' openLinks={openLinks} linkTextStyle={linkTextStyle} />;
+
+  const linksList = linksArr.map(link => (
+    <ListItem key={`listitem-${link.text}`} disablePadding sx={{ display: 'block' }}>
+      <ListItemButton key={`listbutton-${link.text}`}
+        sx={{
+          minHeight: 48,
+          justifyContent: openLinks ? 'initial' : 'center',
+          px: 2.5,
+          backgroundColor: "inherit"
+        }}
+        href={link.link}
+        target="_blank"
+      >
+        <ListItemIcon key={`listicon-${link.text}`}
+          sx={{
+            minWidth: 0,
+            mr: openLinks ? 3 : 'auto',
+            justifyContent: 'center',
+          }}
+        >
+          {link.icon}
+        </ListItemIcon>
+        <ListItemText disableTypography primary={<Typography type="body1" sx={linkTextStyle}>{link.text}</Typography>} sx={{ opacity: openLinks ? 1 : 0 }} />
+      </ListItemButton>
+    </ListItem>
+  ));
 
   return (
     <List>
--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -26,38 +26,10 @@ function MenuContainer(props) {
   const {particles, setParticles} = props;
   const [openLinks, setOpenLinks] = useState(false);
 
-  const themeToggle = <ThemeToggle key='switch-button' openLinks={openLinks} linkTextStyle={linkTextStyle} />;
-
-  const linksList = linksArr.map(link => (
-    <ListItem key={`listitem-${link.text}`} disablePadding sx={{ display: 'block' }}>
-      <ListItemButton key={`listbutton-${link.text}`}
-        sx={{
-          minHeight: 48,
-          justifyContent: openLinks ? 'initial' : 'center',
-          px: 2.5,
-          backgroundColor: "inherit"
-        }}
-        href={link.link}
-        target="_blank"
-      >
-        <ListItemIcon key={`listicon-${link.text}`}
-          sx={{
-            minWidth: 0,
-            mr: openLinks ? 3 : 'auto',
-            justifyContent: 'center',
-          }}
-        >
-          {link.icon}
-        </ListItemIcon>
-        <ListItemText disableTypography primary={<Typography type="body1" sx={linkTextStyle}>{link.text}</Typography>} sx={{ opacity: openLinks ? 1 : 0 }} />
-      </ListItemButton>
-    </ListItem>
-  ));
-
   return (<>
       <CssBaseline />
-      <LinksBar linkTextStyle={linkTextStyle} openLinks={openLinks} setOpenLinks={setOpenLinks} themeToggle={themeToggle} linksList={linksList} drawerWidth={drawerWidth} DrawerHeader={DrawerHeader} particles={particles} setParticles={setParticles} />
-      <LinksMobile linkTextStyle={linkTextStyle} openLinks={openLinks} setOpenLinks={setOpenLinks} themeToggle={themeToggle} linksList={linksList} drawerWidth={drawerWidth} DrawerHeader={DrawerHeader} />
+      <LinksBar linkTextStyle={linkTextStyle} openLinks={openLinks} setOpenLinks={setOpenLinks} particles={particles} setParticles={setParticles} />
+      {/* <LinksMobile linkTextStyle={linkTextStyle} openLinks={openLinks} setOpenLinks={setOpenLinks} /> */}
     </>
   );
 }
