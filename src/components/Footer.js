12ec2d6d3b636132d053515b893b6a74e09f6e6b
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-07-30 18:14:01 -0400

    fit footer alignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import { Typography } from "@mui/material";
 
 function ContentFooter() {
   return (
-    <footer id="content-footer">
-      <Typography variant="h6">
-        <img src="./Pink Sakura Flowers Logo.png" width='32'/> Designed and coded by Sarah Dela Cruz
+    <footer id="content-footer" height="32">
+      <Typography variant="body1" fontWeight="500" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <img src="./Pink Sakura Flowers Logo.png" width='32' />&nbsp;Designed and coded by Sarah Dela Cruz
       </Typography>
     </footer>
   );
