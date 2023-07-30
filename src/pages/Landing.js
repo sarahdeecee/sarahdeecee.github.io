0e8d6890a5b88ab05c0ef6271ca7c8ced3b81649
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-07-30 13:17:01 -0400

    change margins for landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -11,7 +11,7 @@ function Landing(props) {
       <MenuContainer particles={particles} setParticles={setParticles} />
       {/* <article id="bio" width="100%""> */}
       <Grid container id="landing-container" component="article" direction="row" sx={{justifyContent: "space-between", alignItems: "center", width: '100%'}}>
-        <Grid item container xs={12} sx={{justifyContent: 'space-evenly'}}>
+        <Grid item container xs={12} sx={{justifyContent: 'space-evenly', mr: '4em'}}>
           <Box sx={{zIndex: 1, display: 'flex', maxWidth: 'max-content', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start'}}>
             <Typography variant="h4" component="p">Hello! My name is</Typography>
             <Typography variant="h1" component="p" id="landing-name" sx={{fontWeight: "400"}}>Sarah Dela Cruz.</Typography>
