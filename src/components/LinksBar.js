3e019ba284c2a8e8539f595758a5a8641d8faa6c
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-07-31 12:08:44 -0400

    clean code

--- src/components/LinksBar.js
+++ src/components/LinksBar.js
@@ -1,15 +1,7 @@
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
-import { ParticleToggle } from "./ParticleToggle";
 import { linksArr } from "../data/Links";
-import ThemeToggle from "./ThemeToggle";
 
 function LinksBar(props) {
-  const {particles, setParticles} = props;
-
-  const linkTextStyle = {fontSize: '16px', fontWeight: 500};
-
-  const themeToggle = <ThemeToggle key='switch-button' linkTextStyle={linkTextStyle} />;
-
   const linksList = linksArr.map(link => (
     <ListItem key={`listitem-${link.text}`} disablePadding sx={{ display: 'block' }}>
       <ListItemButton key={`listbutton-${link.text}`}
@@ -31,7 +23,6 @@ function LinksBar(props) {
         >
           {link.icon}
         </ListItemIcon>
-        {/* <ListItemText disableTypography primary={<Typography type="body1" sx={linkTextStyle}>{link.text}</Typography>} sx={{ opacity: openLinks ? 1 : 0 }} /> */}
       </ListItemButton>
     </ListItem>
   ));
@@ -39,8 +30,6 @@ function LinksBar(props) {
   return (
     <List id="links-bar" className="button-bar">
       {linksList}
-      {/* {themeToggle} */}
-      {/* <ParticleToggle openLinks={openLinks} particles={particles} setParticles={setParticles} linkTextStyle/> */}
     </List>
   );
 }
